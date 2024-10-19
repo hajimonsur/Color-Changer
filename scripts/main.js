8bbff6b04e733b505fe934381a3a8f7e39798e60
hajimonsur <taiwomonsuru29@gmail.com>
2024-10-19 17:04:10 +0100

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
-// dom content loaded
-document.addEventListener('DOMContentLoaded', () => {
+console.log(document)
+// document.addEventListener('DOMContentLoaded', () => {
    const colorBoxElem = document.getElementById('colorBox');
    const changeBtnElem = document.getElementById('changeBtn');
 
    changeBtnElem.addEventListener('click', () => {
        const randomColor = Math.floor(Math.random()*123456789);
        colorBoxElem.style.backgroundColor = `#${randomColor}`;
+      
        console.log(randomColor)
      
 
    })
-});
+
+
+
 
 // const randomColor = Math.floor(Math.random()*16777215).toString(16);
 // colorBoxElem.style.backgroundColor = `#${randomColor}`;
