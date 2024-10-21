36cc73f3d9330567f7032f2c9ea916e7229768f7
hajimonsur <taiwomonsuru29@gmail.com>
2024-10-21 15:11:01 +0100

    ooooo

--- scripts/main.js
+++ scripts/main.js
@@ -5,10 +5,13 @@
    const changeBtnElem = document.getElementById('changeBtn');
 
    changeBtnElem.addEventListener('click', () => {
-       const randomColor = Math.floor(Math.random()*123456789);
+       const randomColor = Math.floor(Math.random()*16777215).toString(16);
        colorBoxElem.style.backgroundColor = `#${randomColor}`;
-      
-       console.log(randomColor)
+
+ 
+    //    return  `#${randomColor}`;
+
+     console.log(`#${randomColor}`);
      
 
    })
